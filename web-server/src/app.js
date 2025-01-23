41ddacd99c0aed607d3218afd237f07f7f5cbbef
ButrumJ <143197017+JoshuaButrum@users.noreply.github.com>
2025-01-23 12:17:25 -0600

    added a lot of stuff

---
+++ web-server/src/app.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const express = require('express')
+
+console.log(__dirname)
+console.log(path.join(__dirname, 'public'))
+
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>')
+})
+
+app.get('/help', (req, res) => {
+    res.send([{
+        name: 'Andrew',
+    }, {
+        name: 'Sarah'
+    }])
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
